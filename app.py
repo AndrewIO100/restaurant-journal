5b133fc36fa60f682446676e59ed2e94e4095064
Andrew Chiang <andrewjcf@Andrews-MacBook-Pro-4.local>
2021-06-08 20:43:48 -0500

    Added new functions and an endpoint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from chalice import Chalice
 
-from src.controller import restaurant_chooser
+from controller import restaurant_chooser, dynamo_create_review, dynamo_retrieve_reviews
 
 app = Chalice(app_name='restaurant-journal')
 
@@ -10,9 +10,25 @@ def index():
     return {'hello': 'world'}
 
 
+# Endpoint chooses a random restaurant from the parameters given,
+# term = Type of food. eg.Chinese, Italian, American, etc
+# location = City, State
 @app.route('/random-restaurant/{term}/{location}')
 def random_restaurant(term, location):
     return restaurant_chooser(term, location)
+
+
+@app.route('/create_review', methods=['POST'])
+def review_restaurant():
+    # This is the JSON body the user sent in their POST request.
+    review = app.current_request.json_body
+    return dynamo_create_review(review)
+
+
+# Endpoint that gets the user reviews
+@app.route('/get_reviews', methods=['GET'])
+def get_restaurant_reviews():
+    return dynamo_retrieve_reviews()
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
--- controller.py
+++ controller.py
@@ -1,8 +1,13 @@
 import random
+import uuid
 
+import boto3
 import requests
+from boto3.dynamodb.conditions import Key
 
 API_KEY = "dP7ema0d4sMqlW7K-nbZkjwLGrRwGp-26M-FyEtVzacEqFV8Pn3VG_VuhL-NZUshSRT0ZtJhLvlixb53M_fCZ8-xztkAmmbwS1YpOSgKqaGn2xOONrpk02KRQhG4YHYx"
+
+
 def restaurant_chooser(term, location):
     url = f"https://api.yelp.com/v3/businesses/search?term={term}&location={location}"
     headers = {"Authorization": f"Bearer {API_KEY}"}
@@ -13,7 +18,7 @@ def restaurant_chooser(term, location):
     # size of the restaurant list
     list_size = len(businesses_list)
     # chooses a random number to recommend the restaurant
-    restaurant_number = random.randint(0 , list_size)
+    restaurant_number = random.randint(0, list_size)
 
     name = businesses_list[restaurant_number].get("name")
     image_url = businesses_list[restaurant_number].get("image_url")
@@ -28,7 +33,7 @@ def restaurant_chooser(term, location):
         "name": name,
         "image_url": image_url,
         "url": url,
-        "categories":categories,
+        "categories": categories,
         "rating": rating,
         "price": price,
         "location": location,
@@ -36,3 +41,36 @@ def restaurant_chooser(term, location):
 
     return business_dict
 
+
+# Creates the user review and stores it in Dynamodb reviews-logger
+def dynamo_create_review(review):
+    client = boto3.resource('dynamodb')
+    table = client.Table('reviews-logger')
+    user_id = "Andrew"  # TODO: Change later with login
+    restaurant_name = review.get("restaurant_name")
+    response = table.put_item(
+        # Query that stores the review of the user
+        Item={
+            # Used uuid5 instead uuid4 to get rid of duplicates
+            'id': str(uuid.uuid5(uuid.NAMESPACE_URL, f"{user_id}{restaurant_name}")),
+            'user_id': user_id,
+            'restaurant_name': restaurant_name,
+            'title': review.get('title'),
+            'rating': review.get('rating'),
+            'review': review.get('review')
+        }
+    )
+    item = response
+    print(item)
+
+# Retrieves the reviews of the user
+def dynamo_retrieve_reviews():
+    client = boto3.resource('dynamodb')
+    table = client.Table('reviews-logger')
+    # Displays all the reviews of the user
+    response = table.query(
+        IndexName='user_id-index',
+        KeyConditionExpression=Key('user_id').eq('Andrew')
+    )
+
+    return response.get("Items") #response.get() to obtain only the list of reviews
